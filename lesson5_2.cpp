424e6c4eb4cba6b307c9640a96afac9fff20b858
yu1879 <shengweiyukobe0526@gmail.com>
2023-05-20 10:37:12 +0800

    c++

--- lesson5_2.cpp
+++ lesson5_2.cpp
@@ -1,8 +1,13 @@
 ﻿#include  <iostream>
 
 using namespace std;
+typedef char AGE;
 
 int main(int args, char **argv) {
+    AGE myAge=65;
+    AGE score=49;
+    cout<<myAge<<endl;
+    cout<<score<<endl;
 
 
     return 0;
