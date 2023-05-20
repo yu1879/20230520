c7f4295d7ce35839df5a69a398dd07983fed757a
yu1879 <shengweiyukobe0526@gmail.com>
2023-05-20 12:25:48 +0800

    c++

---
+++ lesson5_4.cpp
@@ -0,0 +1,32 @@
+﻿#include  <iostream>
+#include <conio.h>>
+#include "myHeader.h"
+#ifndef  _myHeader_h_
+#endif
+
+using namespace std;
+
+int main(int args, char **argv) {
+    int year ;
+    cout<<"消費西元年份:";
+    cin>>year;
+    if(year>=LASTYEAR){
+        cout<<"終身白金卡會員活動已結束!\n是否轉買線上白金卡一年期?(y/n):";
+        char ch=getche();
+        if(toupper(ch)=='Y'){
+            cout<<endl<<M3<<MSG;
+        }else{
+            cout<<endl<<MSG;
+        }
+    }
+    else if(year>=FIRSTYEAR){
+
+        cout<<M2<<MSG;
+
+    }else{
+        cout<<M1<<MSG;
+
+    }
+
+    return 0;
+}
---
+++ myHeader.h
@@ -0,0 +1,6 @@
+﻿#define FIRSTYEAR 2002
+#define LASTYEAR 2008
+#define M1 "\n\n線上收費--19000元整\n\n"
+#define M2 "\n\n線上收費--27800元整\n\n"
+#define M3 "\n\n線上收費--17600元整\n\n"
+#define MSG "\n\n謝謝光臨，期待再次為您服務\n\n"
