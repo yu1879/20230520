8ae48b61b2fe045c6d63095b48097f28877f560e
yu1879 <shengweiyukobe0526@gmail.com>
2023-05-20 17:04:10 +0800

    c++

--- lesson6_3.cpp
+++ lesson6_3.cpp
@@ -3,7 +3,16 @@
 using namespace std;
 
 int main(int args, char **argv) {
+    fstream fs("hello.txt",ios::in);
+//fs.open("hello.txt");
+//cout<<fs.is_open()<<endl;
+    char *p=new char[100];
+    fs.getline(p,100);
+    cout<<p<<endl;
+//int  n;
+//n=fs.get();
+//cout<<(char)n<<endl;
 
-
+    fs.close();
     return 0;
 }
