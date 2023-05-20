b43f32840b5ab3fbdb000e4a84d8d3434138f8ae
yu1879 <shengweiyukobe0526@gmail.com>
2023-05-20 11:45:11 +0800

    c++

--- lesson5_2.cpp
+++ lesson5_2.cpp
@@ -1,14 +1,55 @@
 ﻿#include  <iostream>
-
+#include  <conio.h>
+#include  <ctime>
+#include  <cstdlib>
 using namespace std;
+#define EQ ==
+#define CEOF 27
+#define CEND "謝謝光臨\n"
+#define SQUARE(x) (x)*(x)
+#define CMAX(a,b) (a>b?a:b)
+#define CMAX3(a,b,c) CMAX(a,CMAX(b,c))
+#define GETRAND(a,b) (rand()%(b-a+1)+a)
+#define CSWAP(a,b) a=a+b;b=a-b;a-=b;
+
 typedef char AGE;
 
 int main(int args, char **argv) {
-    AGE myAge=65;
-    AGE score=49;
-    cout<<myAge<<endl;
-    cout<<score<<endl;
+    int a=100,b=200;
+    CSWAP(a,b);
+    cout<<"a="<<a<<",b="<<b<<endl;
+    //    srand(time(0));
+//    for(int m=1;m<=10;m++){
+//        cout<<char(GETRAND(65,90))<<"\t";
+//    }
+//    cout<<endl;
 
+//cout<<CMAX3(12,52,112)<<endl;
+//cout<<CMAX3(12,52,82)<<endl;
+//
+//cout<<CMAX(12,52)<<endl;
+//cout<<CMAX(92,52)<<endl;
+
+//	cout<<SQUARE(3+3)<<endl;
+//	    cout<<SQUARE((3+3))<<endl;
+
+//char ch;
+//while(ch!= CEOF){
+//	ch=getch();
+//	cout<<ch;
+//}
+//cout<<CEND;
+//int score=101;
+//if(score EQ 100){
+//	cout<<"相等\n";
+//}else{
+//		cout<<"不一樣\n";
+//
+//}
+//AGE myAge=65;
+//AGE score=49;
+//cout<<myAge<<endl;
+//cout<<score<<endl;
 
     return 0;
 }
---
+++ lesson5_3.cpp
@@ -0,0 +1,28 @@
+﻿#include  <iostream>
+#include  <iomanip>
+#include <cmath>
+using namespace std;
+#define PI 3.14159
+#define RANGE(r) r>=1 &&r<=30
+#define CAREA(r) pow(r,2)*PI
+#define CPERIMETER(r) 2*r*PI
+#define ERROR_MSG "SET Range:1 To 30"
+int main(int args, char **argv) {
+    bool flag=true;
+    double r;
+    do{
+        cout<<"設定半徑值:";
+        cin>>r;
+        if(RANGE(r)){
+            flag=false;
+        }else{
+            cout<<ERROR_MSG;
+        }
+    }while(flag);
+    cout<<"\n半徑為:"<<r<<"求得的圓面積為:" <<fixed<<setprecision(2)<< CAREA(r)<<endl;
+    cout<<"\n半徑為:"<<r<<"求得的圓面積為:" <<fixed<<setprecision(2)<< CPERIMETER(r)<<endl;
+
+    return 0;
+
+    return 0;
+}
